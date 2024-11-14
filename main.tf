e6e196813448e27e6de42a26c8f4ac852d29a021
206675896_nbcuni <priyabrata.jena@nbcuni.com>
2024-11-14 15:44:43 +0530

    message

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "google-beta" {
+  project = data.google_project.project.project_id
+  region  = "us-central1"
+}
+
+data "google_project" "project" {}
+
+module "kafka_cluster" {
+  source = "./df_resource/module"  # Update with the actual path to your module
+  project_id = "valid-verbena-437709-h5"
+  cluster_id         = "dataops-kafka"
+  region             = "us-central1"
+  vcpu_count         = 4
+  memory_bytes       = 4294967296
+  subnet             = "projects/valid-verbena-437709-h5/regions/us-central1/subnetworks/default"
+  topic_id           = "dataops-kafka-topic"
+  partition_count    = 3
+  replication_factor = 3
+  cleanup_policy     = "compact"
+}
