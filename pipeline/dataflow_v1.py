c66e3bd7a9a29d4f25b6966c6aa228744fac7d69
206675896_nbcuni <priyabrata.jena@nbcuni.com>
2024-11-15 22:23:17 +0530

    message

--- pipeline/dataflow_v1.py
+++ pipeline/dataflow_v1.py
@@ -19,77 +19,29 @@ import json
 PROJECT_ID = 'valid-verbena-437709-h5'
 DATASET_ID = 'nifty50'
 TABLE_ID = 'nifty50_historical_data_65'
-# Token Provider class
-# This class handles the OAuth token retrieval and formatting
-class TokenProvider(object):
-
-  def __init__(self, **config):
-    self.credentials, _project = google.auth.default()
-    self.http_client = urllib3.PoolManager()
-    self.HEADER = json.dumps(dict(typ='JWT', alg='GOOG_OAUTH2_TOKEN'))
-
-  def valid_credentials(self):
-    if not self.credentials.valid:
-      self.credentials.refresh(google.auth.transport.urllib3.Request(self.http_client))
-    return self.credentials
-
-  def get_jwt(self, creds):
-    return json.dumps(
-        dict(
-            exp=creds.expiry.timestamp(),
-            iss='Google',
-            iat=datetime.datetime.now(datetime.timezone.utc).timestamp(),
-            scope='kafka',
-            sub=creds.service_account_email,
-        )
-    )
-
-  def b64_encode(self, source):
-    return (
-        base64.urlsafe_b64encode(source.encode('utf-8'))
-        .decode('utf-8')
-        .rstrip('=')
-    )
-
-  def get_kafka_access_token(self, creds):
-    return '.'.join([
-      self.b64_encode(self.HEADER),
-      self.b64_encode(self.get_jwt(creds)),
-      self.b64_encode(creds.token)
-    ])
-
-  def token(self):
-    creds = self.valid_credentials()
-    return self.get_kafka_access_token(creds)
-
-  def confluent_token(self):
-    creds = self.valid_credentials()
-
-    utc_expiry = creds.expiry.replace(tzinfo=datetime.timezone.utc)
-    expiry_seconds = (utc_expiry - datetime.datetime.now(datetime.timezone.utc)).total_seconds()
-
-    return self.get_kafka_access_token(creds), time.time() + expiry_seconds
-
-# Confluent does not use a TokenProvider object
-# It calls a method
-def make_token(args):
-    t = TokenProvider()
-    token, _ = t.confluent_token()
-    return token
-
 kafka_cluster_name = 'dataopsguru-kafka'
 region = 'us-central1'
 project_id = 'valid-verbena-437709-h5'
 port = '9092'
 kafka_topic_name = 'dataops-kafka-topic'
 
+# Function to read password from the file
+def read_password_from_file(password_file_path):
+    with open(password_file_path, 'r') as file:
+        return file.read().strip()  # Read password and remove any extra whitespace
+
+# Read the password from 'password.txt'
+password = read_password_from_file('./password.txt')
+
 # Kafka Producer configuration with OAUTHBEARER authentication
 config = {
     'bootstrap.servers': f'bootstrap.{kafka_cluster_name}.{region}.managedkafka.{project_id}.cloud.goog:{port}',
     'security.protocol': 'SASL_SSL',
-    'sasl.mechanisms': 'OAUTHBEARER',
-    'oauth_cb': make_token,
-}
+    'sasl.jaas.config' : 'org.apache.kafka.common.security.plain.PlainLoginModule',
+    'sasl.mechanisms': 'PLAIN',
+    'sasl.username' :"dataops-guru-sa@valid-verbena-437709-h5.iam.gserviceaccount.com" ,
+    'sasl.password' : password 
+    }
 
 table_schema = {
     'fields': [
@@ -143,12 +95,7 @@ with beam.Pipeline(options=pipeline_options) as p:
         p
         # Read from Kafka topic
         | 'Read from Kafka' >> ReadFromKafka(
-            consumer_config= {
-                      'bootstrap.servers': 'bootstrap.dataopsguru-kafka.us-central1.managedkafka.valid-verbena-437709-h5.cloud.goog:9092',
-                      'security.protocol': 'SASL_SSL',
-                      'sasl.mechanisms': 'OAUTHBEARER',
-                      'oauth_cb': make_token,
-                   },
+            consumer_config=config,
             topics=['dataops-kafka-topic'],
             with_metadata=False,
         )
