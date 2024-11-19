e85b8c31181712ae3b4ac096bae071e5288a40b2
206675896_nbcuni <priyabrata.jena@nbcuni.com>
2024-11-19 18:30:54 +0530

    token

--- pipeline/dataflow_v4.py
+++ pipeline/dataflow_v4.py
@@ -26,7 +26,7 @@ pipeline_options = PipelineOptions(
     streaming=True,  #Enable streaming mode
     #Dataflow parameters that are optional
     job_name='streaming-kafka-bq-nifty50-v6',  #Set the Dataflow job name here
-    num_workers=2,  #Specify the number of workers
+    num_workers=1,  #Specify the number of workers
     max_num_workers=10,  #Specify the maximum number of workers
     disk_size_gb=100,  #Specify the disk size in GB per worker
     autoscaling_algorithm='THROUGHPUT_BASED',  #Specify the autoscaling algorithm
